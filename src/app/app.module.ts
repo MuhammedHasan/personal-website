75c7e8a5735bd7f08e4096291d07e34946647d85
Muhammed Hasan Celik <hasancelik@std.sehir.edu.tr>
2017-05-22 22:47:39 +0300

    fist blog post

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MaterialModule } from '@angular/material';
+import { MarkdownModule } from 'angular2-markdown';
 
 import { appRoutingProviders, routing } from './app.routes';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { BlogComponent } from './blog/blog.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    BlogComponent
+    BlogComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
@@ -22,6 +26,7 @@ import { BlogComponent } from './blog/blog.component';
     HttpModule,
     MaterialModule,
     FlexLayoutModule,
+    MarkdownModule.forRoot(),
     routing
   ],
   providers: [appRoutingProviders],
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,15 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import {HomeComponent} from './home/home.component';
-import {BlogComponent} from './blog/blog.component';
+import { HomeComponent } from './home/home.component';
+import { BlogComponent } from './blog/blog.component';
+import { PostComponent } from './post/post.component';
 
 
 const appRoutes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'blog', component: BlogComponent },
+  { path: 'blog/extracting-words-in-a-string-without-spaces', component: PostComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
 
